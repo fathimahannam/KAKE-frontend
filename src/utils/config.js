6f327a139c768b60301d9d1488588b3b2e845af5
fathimahannam <122714714+fathimahannam@users.noreply.github.com>
2023-12-09 21:03:40 +0400

    commit check2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = 'kake-frontend-hannas-projects-70800ae4.vercel.app'
+export const BACKEND_BASE_URL = 'https://kake-frontend.vercel.app/'
 // export const BaseUrl = 'http://127.0.0.1:8000'
 
 export const RAZORPAY_KEY_ID = 'rzp_test_tgCvOVZCwscP6c'
