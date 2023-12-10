7b49dbe3bfd7ba28efa5eca9d70251a4813b8f25
fathimahannam <122714714+fathimahannam@users.noreply.github.com>
2023-12-10 17:38:10 +0400

    check1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-// export const BACKEND_BASE_URL = 'https://kakeapp.onrender.com'
-export const BACKEND_BASE_URL = 'http://127.0.0.1:8000'
+export const BACKEND_BASE_URL = 'https://kakeapp.onrender.com'
+// export const BACKEND_BASE_URL = 'http://127.0.0.1:8000'
 
 export const RAZORPAY_KEY_ID = 'rzp_test_tgCvOVZCwscP6c'
 export const wsApiUrl  = 'wss://kakeapp.onrender.com'
