cc7602ad79a8bff8e0d604b3ec80fab239762407
fathimahannam <122714714+fathimahannam@users.noreply.github.com>
2024-03-02 07:50:59 +0400

    admin ui update

--- src/components/admin/AdminPanel.jsx
+++ src/components/admin/AdminPanel.jsx
@@ -98,8 +98,18 @@ function AdminPanel() {
       </button>
 
       {/* Main Content */}
+      <div className="flex-1 ">
+      <div className="bg-cover bg-center w-full h-full absolute z-0" style={{ backgroundImage: "url('https://64.media.tumblr.com/c4dfaa40ebc2def7df57d6b80cc37948/tumblr_peeip0iNXh1tdnbbbo1_540.gif')" }}>
+        {/* Text overlay */}
+        <div className="relative z-10 p-8">
+          <h1 className="text-4xl font-bold text-amber-900">Welcome to the admin portal</h1>
+          <p className="text-lg mt-4 text-amber-900">where we empower you to manage our delicious offerings with ease and precision.</p>
+        </div>
+      </div>
+    </div>
 
     </div>
+    
   
     </div>
   );
